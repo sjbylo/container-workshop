9746dde8306c34f4255d159310a4978aa06c454a
Stephen <stephenbylo@gmail.com>
2019-02-19 10:09:05 +0630

    Update

--- admin/setup.sh
+++ admin/setup.sh
@@ -3,21 +3,21 @@
 # Set the OCP API hostname (master)
 MASTER_SVR=https://master.yangon-002b.openshiftworkshop.com
 
-# Set up 100 users: user0 to user99
-for i in {0..99}; do useradd user$i \
+# Set up 200 users: user0 to user199
+for i in {0..199}; do useradd user$i \
   -p \
 '$6$qmGzVnys$7EGA8ngfhQnHGvNTkeODC6Kw8VvABOYDAfhPua1wSjZJLpDfKi.9g8SAUqLxIyBY5hknSiiXbEZIj5jqgT6Oy.'\
 || break; done
 
 # Log in 100 users
-for i in {0..99}; do   su - user$i -c 'oc login --insecure-skip-tls-verify -u user'$i' -p openshift
+for i in {0..199}; do   su - user$i -c 'oc login --insecure-skip-tls-verify -u user'$i' -p openshift
 \
 '$MASTER_SVR || break; done
 
-for i in {0..99}; do su - user$i -c 'oc whoami' || break; done
+for i in {0..199}; do su - user$i -c 'oc whoami' || break; done
 
 # Delete all
-for i in {0..99}; do userdel -r user$i || break; done
+for i in {0..199}; do userdel -r user$i || break; done
 
 # Allow sshd password auth
 sed -i "s/^PasswordAuthentication/#PasswordAuthentication/g"  /etc/ssh/sshd_config
